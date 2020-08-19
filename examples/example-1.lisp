c79d2aade57c993f0634efe7eb8903c19aaa05fc
Markus Aschl <mail@markusaschl.com>
2020-08-19 11:05:50 +0200

    add example usage

--- examples/example-1.lisp
+++ examples/example-1.lisp
@@ -1,11 +1,40 @@
+(uiop:define-package :cloudless/libraries/binary-parser/examples
+  (:use :common-lisp)
+  (:use :binary-parser)
+  (:nicknames :binary-parser/examples))
+
+
+(in-package :binary-parser/examples)
+
+
 (binary-parser:define-binary-struct
     (message
      (:conc-name message--)
      (:constructor %make-message))
 
-    (signature 0 :type (unsigned-integer 16) :binary-type unsigned-integer16)
-  (protocol 0 :type (unsigned-integer 16) :binary-type unsigned-integer16)
-  (version 0 :type (unsigned-integer 16) :binary-type unsigned-integer16)
-  (id 0 :type (unsigned-integer 16) :binary-type unsigned-integer16)
-  (nonce 0 :type (unsigned-integer 64) :binary-type unsigned-integer64)
-  (chunk nil :type (octet-vector *) :binary-type octet-array))
+    (signature 0 :type (unsigned-integer 16) :binary-type :unsigned-integer16)
+  (protocol 0 :type (unsigned-integer 16) :binary-type :unsigned-integer16)
+  (version 0 :type (unsigned-integer 16) :binary-type :unsigned-integer16)
+  (id 0 :type (unsigned-integer 16) :binary-type :unsigned-integer16)
+  (nonce 0 :type (unsigned-integer 64) :binary-type :unsigned-integer64)
+  (chunk (make-array 0 :element-type 'octet) :type (octet-vector *) :binary-type :octet-array))
+
+
+(defparameter *message* (%make-message :signature 123
+                                       :protocol 3000
+                                       :version 1
+                                       :id 435))
+
+(with-open-file (out "message.bin"
+                     :direction :output
+                     :if-exists :supersede
+                     :element-type '(unsigned-byte 8))
+
+  (write-value :message out *message*))
+
+
+(with-open-file (in "message.bin"
+                     :direction :input
+                     :element-type '(unsigned-byte 8))
+
+  (format t "~S~%" (read-value :message in)))
