eb863851dbecfc254897b3dc5ba0a5b42522c18f
Markus Aschl <mail@markusaschl.com>
2020-08-19 09:22:13 +0200

    export all symbols

--- src/package.lisp
+++ src/package.lisp
@@ -1,13 +1,38 @@
 (uiop:define-package :cloudless/libraries/binary-parser
   (:use :common-lisp)
   (:nicknames :binary-parser)
+
+  ;; constants
   (:export #:+max-octet-array-length+
-           #:octet
-           #:octet-vector
-           #:unsigned-integer
-           #:unsigned-integer
-           #:read-value
-           #:write-value
-           #:define-binary-type
+           #:define-binary-struct)
+
+  ;; pure lisp types
+  (:export
+   #:octet
+   #:octet-vector
+   #:unsigned-integer)
+
+  ;; accessors
+  (:export #:read-value
+           #:write-value)
+
+  ;; macros
+  (:export #:define-binary-type
            #:define-binary-enum
-           #:define-binary-struct))
+           #:define-binary-struct)
+
+  ;; binary types
+  (:export #:unsigned-integer16
+           #:integer16
+           #:unsigned-integer32
+           #:integer32
+           #:unsigned-integer64
+           #:integer64
+           #:binary-boolean
+           #:real32
+           #:real64
+           #:octet
+           #:binary-string
+           #:fixed-array
+           #:octet-array
+           #:binary-enum))
